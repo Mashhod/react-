887f2f9bea45e8bb9ef858d3bb901cd2c7178e51
Mashhod Bin Shahid <mashhodbinshahid11@gmail.com>
2025-01-19 22:32:57 +0500

    Add files via upload

---
+++ Routing/src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const About = () => {
+    return(
+        <div style={{fontSize: "35", height: "100vh", backgroundColor: "Yellow"}} >About</div>
+
+    )
+
+
+}
+
+export default About;
